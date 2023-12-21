f3baad99791b8799e29346355d96207e18a563e5
Dinh Tuans <dinhmanhtuans89@gmail.com>
2023-12-21 15:43:59 +0700

    Add project files.

---
+++ PokemonReviewApp/Helper/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Helper
+{
+    public class MappingProfiles:Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+            CreateMap<Category, CategoryDto>();
+            CreateMap<Country, CountryDto>();
+            CreateMap<Owner, OwnerDto>();
+        }
+    }
+}
